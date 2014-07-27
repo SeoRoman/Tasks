a1fd48a0a5cb390add41395f72862c997b42677f
SeoRoman <roman.lopez@roosterteeth.com>
2014-07-27 01:18:56 -0500

    blah

--- public/views/dashboard/index.blade.php
+++ public/views/dashboard/index.blade.php
@@ -27,7 +27,7 @@
 			<div class="columnOne" ng-repeat="task in data.columnOne">
 
 	            <div class="panel panel-info">
-	              <div class="panel-heading">{{ task.title }} <span class="pull-right"><a href="javascript:void();" ng-click="togglePanel(task)"><i ng-hide="task.show" class="fa fa-plus"></i><i ng-show="task.show" class="fa fa-minus"></i></a></span></div>
+	              <div class="panel-heading"><a href="javascript:void();" ng-click="togglePanel(task)">{{ task.title }} <span class="pull-right"><i ng-hide="task.show" class="fa fa-plus"></i><i ng-show="task.show" class="fa fa-minus"></i></span></a></div>
 	              <div class="panel-body" ng-show="task.show" ng-animate="customClass">
 	                Panel content
 	              </div>
