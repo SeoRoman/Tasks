206d2284f31edef543f9e5dd04e337a0a5ad28ec
SeoRoman <roman.lopez@roosterteeth.com>
2014-08-08 17:24:43 -0500

    Not needed, not using angular method

--- public/angularjs/modules/TaskList/controllers/TaskListController.js
+++ public/angularjs/modules/TaskList/controllers/TaskListController.js
@@ -35,8 +35,6 @@ angular.module('TaskList').controller('TaskListController', function($rootScope,
 
 		var task = data.task.data;
 
-		if (!$scope.tasklist.$visible) $scope.tasklist.$visible = true;
-
 		$scope.tasklist.tasks.push(task);
 
 	});
