00242b1a78c6f916861f63955a74f1c56db2a362
SeoRoman <roman.lopez@roosterteeth.com>
2014-08-09 19:25:28 -0500

    Added Wait Dialog to Create New TaskLIst

--- public/angularjs/modules/TaskList/controllers/TaskListStoreController.js
+++ public/angularjs/modules/TaskList/controllers/TaskListStoreController.js
@@ -13,8 +13,11 @@ angular.module('TaskList').controller('TaskListStoreController', function($rootS
 
 		tasklist.tasks_projects_id = data.ProjectID;
 
+		Dialog.loading('tasklist-create');
+
 		TaskList.save(tasklist, { ProjectID: data.ProjectID }, function(tasklist) {
 			$modalInstance.close();
+			Dialog.close('tasklist-create');
 			$rootScope.$broadcast('tasklist-create', { tasklist: tasklist });
 		});
 
